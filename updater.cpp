3fd8f78ebd666eb97ad5fccedd74c6fe83e60d2b
Nick Veys <nickveys@gmail.com>
2014-01-16 18:25:24 -0600

    Progress on building the updates

--- updater.cpp
+++ updater.cpp
@@ -18,35 +18,46 @@ void Updater::update()
     qDebug() << "Updater::update()";
 
     QList<HostInformation> hosts;
+    QString token;
 
-    QSettings settings;
-    int size = settings.beginReadArray("monitored-hosts");
-    for (int i = 0; i < size; ++i) {
-        settings.setArrayIndex(i);
-        QString name = settings.value("name").toString();
-        QString host = settings.value("host").toString();
-        QString port = settings.value("port").toString();
+    {
+        QSettings settings;
+        int size = settings.beginReadArray("monitored-hosts");
+        for (int i = 0; i < size; ++i) {
+            settings.setArrayIndex(i);
+            QString name = settings.value("name").toString();
+            QString host = settings.value("host").toString();
+            QString port = settings.value("port").toString();
 
-        /* skip incomplete entries */
-        if (name.isEmpty() || host.isEmpty() || port.isEmpty())
-        {
-            continue;
+            /* skip incomplete entries */
+            if (name.isEmpty() || host.isEmpty() || port.isEmpty())
+            {
+                continue;
+            }
+
+            bool ok = true;
+            HostInformation info = { name, host, port.toUShort(&ok) };
+
+            if (ok)
+            {
+                hosts.append(info);
+                qDebug() << "Appended: " << info.name << ", " << info.host << ", " << info.port;
+            }
+            else
+            {
+                // XXX notify port error?
+            }
         }
+        settings.endArray();
 
-        bool ok = true;
-        HostInformation info = { name, host, port.toUShort(&ok) };
-
-        if (ok)
-        {
-            hosts.append(info);
-            qDebug() << "Appended: " << info.name << ", " << info.host << ", " << info.port;
-        }
-        else
-        {
-            // XXX notify port error?
-        }
+        token = settings.value("api-key").toString();
     }
-    settings.endArray();
+
+    if (token.isEmpty())
+    {
+        qDebug() << "TOKEN EMPTY, skipping update";
+        return;
+    }
 
     qDebug() << "Built host list, " << hosts.length() << " entries";
 
@@ -60,14 +71,38 @@ void Updater::update()
             QJsonDocument devs = api.devs();
             QJsonDocument pools = api.pools();
 
-            QJsonObject vobj = version.object();
-            QJsonObject vobj2 = vobj["VERSION"].toArray()[0].toObject();
-
             QJsonObject update;
             update["host"] = info.name;
             update["uptime"] = summary.object()["SUMMARY"].toArray()[0].toObject()["Elapsed"];
             update["mhash"] = summary.object()["SUMMARY"].toArray()[0].toObject()["MHS av"];
             update["rejectpct"] = summary.object()["SUMMARY"].toArray()[0].toObject()["Pool Rejected%"];
+
+            QJsonArray gpus, asics, fpgas;
+            Q_FOREACH (QJsonValue value, devs.object()["DEVS"].toArray())
+            {
+                QJsonObject devObj = value.toObject();
+
+                if (devObj.contains("GPU"))
+                {
+                    QJsonObject gpu;
+                    gpu["index"] = devObj["GPU"];
+                    gpu["temperature"] = devObj["Temperature"];
+                    gpu["enabled"] = devObj["Enabled"].toString() == "Y";
+                    gpu["status"] = devObj["Status"];
+                    gpu["uptime"] = devObj["Device Elapsed"];
+                    gpu["mhash"] = devObj["MHS av"];
+                    gpu["hwerrors"] = devObj["Hardware Errors"];
+                    gpu["rejectpct"] = devObj["Device Rejected%"];
+                    gpus.append(gpu);
+                }
+            }
+            update["gpus"] = gpus;
+            update["asics"] = asics;
+            update["fpgas"] = fpgas;
+
+            // XXX pools
+
+            // XXX use VERSION information
 
             QJsonObject agent;
             agent["name"] = QString("brigade-monitor-qt");
@@ -79,7 +114,7 @@ void Updater::update()
         }
         catch (std::exception& e)
         {
-            // XXX bark
+            qDebug() << "An exception occurred:" << e.what();
         }
     }
 }
